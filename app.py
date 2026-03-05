69c87d24ae1ede5ca51cbe53bcf8fb03f911df39
Constructionsux <huntclara.56@gmail.com>
2026-03-04 20:01:19 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,29 @@
+import mysql.connector 
+
+def get_db():
+  return mysql.connector.connect(
+        host = os.getenv("DB_HOST"),
+        user =  os.getenv("DB_USER"),
+        password =  os.getenv("DB_PASSWORD"),
+        database =  os.getenv("DB_NAME"), 
+        port =  os.getenv("DB_PORT"),
+)
+
+
+def check_overdue_invoices():
+     conn = get_db()
+     cursor = conn.cursor()
+
+     try:
+          cursor.execute(
+          """
+          UPDATE invoices
+          SET status='overdue'
+          WHERE due_date < NOW() AND status IN ('pending', 'unpaid')
+          """
+          )
+          conn.commit()
+          print("Overdue invoices updated")
+     finally:
+          cursor.close()
+          conn.close()
